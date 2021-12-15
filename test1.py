d08a113e1d5415964c98c98cb8f0efa15fccd163
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2021-12-15 18:05:37 +0300

    3 commit

--- test1.py
+++ test1.py
@@ -1,2 +1,15 @@
-import requests
+# https://habr.com/ru/sandbox/151294/
 
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://militera.lib.ru/1/cats/all/a/index.html"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'lxml')
+
+soup = soup.encode()
+
+with open('test.html', 'wb') as file:
+    file.write(soup)
