abb0b705dbb763118b720b4d4055799c8f9ce3cf
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2021-12-15 15:05:14 +0300

    3 commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 url = "https://scrapingclub.com/exercise/list_basic/?page=1"
@@ -9,5 +10,17 @@ soup = BeautifulSoup(response.text, 'lxml')
 
 items = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
 
-for item in items:
-    print(item.text)
+for i, item in enumerate(items, start=1):
+    itemName = item.find('h4', class_='card-title').text.strip()
+    itemPrice = item.find('h5').text
+    itemLink = item.find('a')
+    print(f"{i}: {itemName},{itemPrice},{itemLink}")
+
+# with open("results.csv", "w", newline='') as csvfile:
+#     writer = csv.writer(csvfile, delimiter="|")
+#     writer.writerow(["N","item","price","link"])
+#     for i, item in enumerate(items, start=1):
+#         itemName = item.find('h4', class_='card-title').text
+#         itemPrice = item.find('h5').text
+#         itemLink = item.find('a').text
+#         writer.writerow([i, itemName, itemPrice])
