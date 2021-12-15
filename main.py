b6b9a8d13b5cd1eebad5bd96751259abbc51c24c
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2021-12-15 15:19:03 +0300

    3 commit

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ soup = BeautifulSoup(response.text, 'lxml')
 
 items = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
 
-for i, item in enumerate(items, start=1):
-    itemName = item.find('h4', class_='card-title').text.strip()
-    itemPrice = item.find('h5').text
-    itemLink = item.find('a')
-    print(f"{i}: {itemName},{itemPrice},{itemLink}")
+# for i, item in enumerate(items, start=1):
+#     itemName = item.find('h4', class_='card-title').text.strip()
+#     itemPrice = item.find('h5').text
+#     itemLink = "https://scrapingclub.com" + item.find('a')['href']
+#     print(f"{i}: {itemName},{itemPrice},{itemLink}")
 
-# with open("results.csv", "w", newline='') as csvfile:
-#     writer = csv.writer(csvfile, delimiter="|")
-#     writer.writerow(["N","item","price","link"])
-#     for i, item in enumerate(items, start=1):
-#         itemName = item.find('h4', class_='card-title').text
-#         itemPrice = item.find('h5').text
-#         itemLink = item.find('a').text
-#         writer.writerow([i, itemName, itemPrice])
+with open("results.csv", "w", newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter="|")
+    writer.writerow(["N","item","price","link"])
+    for i, item in enumerate(items, start=1):
+        itemName = item.find('h4', class_='card-title').text.strip()
+        itemPrice = item.find('h5').text
+        itemLink = "https://scrapingclub.com" + item.find('a')['href']
+        writer.writerow([i, itemName, itemPrice, itemLink])
